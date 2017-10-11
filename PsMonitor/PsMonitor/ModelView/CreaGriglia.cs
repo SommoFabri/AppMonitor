14d86b002d2f3351e058f633d70d55c2da5a7abe
AlRivoli <italia92>
2017-10-11 15:20:20 +0200

    sommo

--- PsMonitor/PsMonitor/ModelView/CreaGriglia.cs
+++ PsMonitor/PsMonitor/ModelView/CreaGriglia.cs
@@ -57,8 +57,6 @@ namespace PsMonitor.ModelView
         {
             int row = 0;
             int column = 0;
-            int row_image = 0;
-            int column_image = 0;
 
             foreach ( var i in totali.righe)
             {
@@ -84,85 +82,84 @@ namespace PsMonitor.ModelView
                 };
                 var stack_vertical = new StackLayout
                 {
-                    Orientation = StackOrientation.Vertical
-                    
-                    
+                    Orientation = StackOrientation.Vertical,
+                    BackgroundColor=Color.LightGray
                 };
                 var stack_Due = new StackLayout
                 {
                     Orientation = StackOrientation.Horizontal
-                 
-
                 };
                 gridLayout.Children.Add(label, column, row);
                 gridLayout.Children.Add(stack_vertical, column, row);
                 stack_vertical.Children.Add(stack_Due);
-
-                //creo griglia per cerchietti
-                var grid_image = new Grid
-                {
-                    HorizontalOptions = LayoutOptions.Center,
-                      VerticalOptions = LayoutOptions.Center
-                    };
-                grid_image.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1,GridUnitType.Star) });
-                for (int y=0; y<4; y++)
-                {
-                    grid_image.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1,GridUnitType.Star) });
-                }
-
-                for (int y = 0; y < 4; y++)
-                {
-                    string immagine="";
-                    string numero = "";
-                    switch (y)
-                    {
-                        case 0:
-                            immagine = "cerchio_b";
-                            numero = i.bianchi.ToString();
-                            break;
-                        case 1:
-                            immagine = "cerchio_v";
-                            numero = i.verdi.ToString();
-                            break;
-                        case 2:
-                            immagine = "cerchio_g";
-                            numero = i.gialli.ToString();
-                            break;
-                        case 3:
-                            immagine = "cerchio_r";
-                            numero = i.rossi.ToString();
-                            break;
-
-                    }
-                    var image = new Image
-                    {
-                        HorizontalOptions = LayoutOptions.Center,
-                        VerticalOptions = LayoutOptions.Center,
-                        Source = immagine,
-                        Aspect=Aspect.AspectFill
-                    };
-                    var label_image = new Label
-                    {
-                        Text = numero,
-                        HorizontalTextAlignment = TextAlignment.Center,
-                        VerticalTextAlignment = TextAlignment.Center,
-                        TextColor = Color.Black,
-                    };
-                    grid_image.Children.Add(image, column_image, row_image);
-                    grid_image.Children.Add(label_image, column_image, row_image);
-                    column_image++;
-                }
-                column_image = 0;
-                
+                Grid grid_image=griglia_cerchi(i);
                 gridLayout.Children.Add(grid_image, column, row);
                 stack_vertical.Children.Add(grid_image);
                 stack_Due.Children.Add(label);
-                row++;
-                
+                row++;   
+            }
+        }
+
+        public static Grid griglia_cerchi (RigaBean i)
+        {
+            int row_image = 0;
+            int column_image = 0;
+            //creo griglia per cerchietti
+            var grid_image = new Grid
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            grid_image.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            for (int y = 0; y < 4; y++)
+            {
+                grid_image.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
 
+            for (int y = 0; y < 4; y++)
+            {
+                string immagine = "";
+                string numero = "";
+                switch (y)
+                {
+                    case 0:
+                        immagine = "cerchio_b";
+                        numero = i.bianchi.ToString();
+                        break;
+                    case 1:
+                        immagine = "cerchio_v";
+                        numero = i.verdi.ToString();
+                        break;
+                    case 2:
+                        immagine = "cerchio_g";
+                        numero = i.gialli.ToString();
+                        break;
+                    case 3:
+                        immagine = "cerchio_r";
+                        numero = i.rossi.ToString();
+                        break;
 
-
+                }
+                var image = new Image
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    Source = immagine,
+                    Aspect = Aspect.AspectFill
+                };
+                var label_image = new Label
+                {
+                    Text = numero,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    TextColor = Color.Black,
+                };
+                grid_image.Children.Add(image, column_image, row_image);
+                grid_image.Children.Add(label_image, column_image, row_image);
+                column_image++;
+            }
+            column_image = 0;
+            return grid_image;
         }
     }
 }
